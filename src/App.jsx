86583c9c90a5375d3e725346c33211f97755adc0
Kaya <kaya_chen@outlook.com>
2025-01-04 23:08:41 +0800

    <初始化> 環境測試

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,37 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
           <br />
-          <h0 className="gradient-blue">全包服務，無憂旅程</h0>
+          <h0 className="shadow-banner-text">全包服務，無憂旅程</h0>
+          <br />
+          <button className='btn btn-outline-primary-600'>測試</button>
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <nav aria-label="...">
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a className="page-link page-link-custom">Previous</a>
+        </li>
+        <li className="page-item"><a className="page-link" href="#">1</a></li>
+        <li className="page-item active" aria-current="page">
+          <a className="page-link page-link-custom" href="#">2</a>
+        </li>
+        <li className="page-item"><a className="page-link" href="#">3</a></li>
+        <li className="page-item">
+          <a className="page-link page-link-custom" href="#">Next</a>
+        </li>
+      </ul>
+</nav>
+    <div className="card card-custom">
+      {/* <img src="..." className="card-img-top" alt="..." /> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and content.</p>
+        <a href="#" className="btn btn-primary-600">Go somewhere</a>
+      </div>
+    </div>
     </>
   )
 }
