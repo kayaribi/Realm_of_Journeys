31559a5134b8a392edc5ed5d30fe02af5f018627
Kaya <kaya_chen@outlook.com>
2025-02-09 02:24:31 +0800

    feat(FAQ): 增加首頁元件FAQ並完成

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Evaluate from "../components/Evaluate"
+import FAQ from "../components/FAQ"
 
 export default function Home() {
   return (
     <>
       <Evaluate />
+      <FAQ />
     </>
   )
 }
