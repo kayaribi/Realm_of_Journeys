81a16e0b9e6c69a4276e6b21a822b18212c6f057
Kaya <kaya_chen@outlook.com>
2025-02-09 23:11:02 +0800

    fix(BackTopBtn):功能修改完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import {Route,Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // 匯入頁面(pages>index.jsx做匯入喔)
-import {Home,About,Cart,Account,TravelGuide,TravelSpots,TouristSpots,TouristIndex} from './pages';
+import { Home, About, Cart, Account, TravelGuide, TravelSpots, TouristSpots, TouristIndex } from './pages';
 // 匯入元件(去components>index.jsx做匯入喔)
-import {BackTopBtn,Footer,Navbar} from './components';
+import { BackTopBtn, Footer, Navbar } from './components';
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -13,21 +13,21 @@ function App() {
   return (
     <div>
       <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>                    {/* 首頁 */}
-          <Route path='/about' element={<About/>}></Route>               {/* 關於我們 */}
-          <Route path='/touristSpots' element={<TouristSpots/>}>         {/* 旅遊景點 */}
-            <Route index element={<TouristIndex/>}></Route>                 {/* ---商品-呈現 */}
-          </Route>
-          <Route path='/guide' element={<TravelGuide/>}></Route>               {/* 攻略指南 */}
-          <Route path='/cart' element={<Cart/>}></Route>                 {/* 購物車 */}
-          <Route path='/account' element={<Account/>}></Route>           {/* 登入註冊 */}
-        </Routes>
-        <BackTopBtn />
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>                    {/* 首頁 */}
+        <Route path='/about' element={<About />}></Route>               {/* 關於我們 */}
+        <Route path='/touristSpots' element={<TouristSpots />}>         {/* 旅遊景點 */}
+          <Route index element={<TouristIndex />}></Route>                 {/* ---商品-呈現 */}
+        </Route>
+        <Route path='/guide' element={<TravelGuide />}></Route>               {/* 攻略指南 */}
+        <Route path='/cart' element={<Cart />}></Route>                 {/* 購物車 */}
+        <Route path='/account' element={<Account />}></Route>           {/* 登入註冊 */}
+      </Routes>
+      <BackTopBtn />
+      <Footer />
     </div>
-    )
-  }
+  )
+}
 
-  export default App
+export default App
 
