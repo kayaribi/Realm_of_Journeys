cb7bd24b019152765c6fc44017d3a6eba48e702e
倫倫 <f5badapple@gmail.com>
2025-02-09 14:58:41 +0800

    feat(travelSpots):更新旅遊景點頁面，第七版(篩選功能雛形)

--- src/pages/TravelSpots.jsx
+++ src/pages/TravelSpots.jsx
@@ -13,16 +13,22 @@ export default function TravelSpots() {
   const [productList, setProductList] = useState([]);
   const [pagination, setPagination] = useState({});
   const [bannerChange, setBannerChange] = useState(productPageBanner);
+  // 客製化篩選用資料
   const [selected, setSelected] = useState("全部");
-  const [initialProducts, setInitialProducts] = useState({}); // 儲存最初的50筆資料
+  const [initialAllProducts, setInitialAllProducts] = useState({}); // 儲存最初的50筆物件資料
+  const [filteredProductData, setFilteredProductData] = useState([]); // 儲存篩選後的陣列資料
+  const [isFilterProducts, setIsFilterProducts] = useState(false);
+  const [cusCurrentPage, setCusCurrentPage] = useState(1);
+  const [cusTotalPages, setCusTotalPages] = useState(1);
+  const itemsPerPage = 10;
 
   const [allProducts, setAllProducts] = useState([]);
   // const [firstHalf, setFirstHalf] = useState(1);
   // const [secondHalf, setSecondHalf] = useState("");
 
-  let firstHalf = 1;
-  let secondHalf = 5;
-  let half = 3;
+  // let firstHalf = 1;
+  // let secondHalf = 5;
+  // let half = 3;
 
   // 登入功能
   const signIn = async () => {
@@ -36,7 +42,7 @@ export default function TravelSpots() {
       // document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
       axios.defaults.headers.common["Authorization"] = token;
 
-      copyInitialProducts();
+      copyInitialAllProducts();
       getProduct();
       // getAllProduct();
     } catch (error) {
@@ -45,14 +51,14 @@ export default function TravelSpots() {
   };
 
   // 備份初始資料功能
-  const copyInitialProducts = async () => {
+  const copyInitialAllProducts = async () => {
     try {
       const res = await axios.get(
         `${BASE_URL}/v2/api/${API_PATH}/admin/products/all`
       );
 
-      if (Object.keys(initialProducts).length === 0) {
-        setInitialProducts(res.data.products);
+      if (Object.keys(initialAllProducts).length === 0) {
+        setInitialAllProducts(res.data.products);
       }
     } catch (error) {
       console.error(error); // 處理錯誤
@@ -66,55 +72,54 @@ export default function TravelSpots() {
 
   // 初始資料備份完成時執行
   useEffect(() => {
-    if (Object.keys(initialProducts).length !== 0) {
-      console.log(initialProducts, "備份完成");
+    if (Object.keys(initialAllProducts).length !== 0) {
+      // 50筆原始物件資料轉陣列
+      setFilteredProductData(Object.values(initialAllProducts));
     }
-  }, [initialProducts]);
+  }, [initialAllProducts]);
 
-  // const getAllProduct = async () => {
-  //   const res = await axios.get(
-  //     `${BASE_URL}/v2/api/${API_PATH}/admin/products/all`
-  //   );
-
-  //   if (Object.keys(initialProducts).length === 0) {
-  //     console.log("我是空物件");
-  //   }
-
-  // setAllProducts(Object.values(res.data.products));
-  // };
-
-  const handleBannerBG = async (e, category) => {
+  const handleFilterProducts = async (e, category) => {
     e.preventDefault();
     setSelected(category);
-    let filteredProductsList = [];
 
-    if (e.target.textContent === "全部") {
+    if (category === "全部") {
       setBannerChange(productPageBanner);
       await getProduct();
-
+      setIsFilterProducts(false);
       return;
-    } else if (e.target.textContent === "亞洲") {
+    }
+
+    setIsFilterProducts(true);
+    let filteredProductsList = [];
+
+    if (category === "亞洲") {
       setBannerChange(productPageBanner2);
 
-      filteredProductsList = allProducts.filter((item) =>
+      filteredProductsList = Object.values(initialAllProducts).filter((item) =>
         item.category.includes("亞洲")
       );
-    } else if (e.target.textContent === "歐洲") {
+    } else if (category === "歐洲") {
       setBannerChange(productPageBanner3);
 
-      filteredProductsList = allProducts.filter((item) =>
+      filteredProductsList = Object.values(initialAllProducts).filter((item) =>
         item.category.includes("歐洲")
       );
-    } else if (e.target.textContent === "中東") {
+    } else if (category === "中東") {
       setBannerChange(productPageBanner4);
 
-      filteredProductsList = allProducts.filter((item) =>
+      filteredProductsList = Object.values(initialAllProducts).filter((item) =>
         item.category.includes("中東")
       );
     }
 
+    setFilteredProductData(filteredProductsList);
+
     // filterProducts(1, filteredProductsList);
   };
+
+  useEffect(() => {
+    console.log(filteredProductData);
+  }, [filteredProductData]);
 
   const getProduct = async (page = 1) => {
     try {
@@ -131,25 +136,21 @@ export default function TravelSpots() {
     }
   };
 
-  // useEffect(() => {
-  //   console.log(initialProducts);
-  // }, [initialProducts]);
-
   // const filterProducts = async (page, filteredProductsList) => {
   //   const startIdx = (page - 1) * 10;
   //   const endIdx = page * 10;
   //   const filteredProducts = filteredProductsList.slice(startIdx, endIdx);
 
-  //   setProductList(filteredProducts);
+  //   // setProductList(filteredProducts);
 
   //   //  根據篩選後的數量，手動更新 pagination
-  //   setPagination({
-  //     total_pages: Math.ceil(filteredProductsList.length / 10), // 每頁 10 筆
-  //     current_page: 1, // 預設從第 1 頁開始
-  //     has_pre: page > 1,
-  //     has_next: page < Math.ceil(filteredProductsList.length / 10),
-  //     category: "",
-  //   });
+  //   // setPagination({
+  //   //   total_pages: Math.ceil(filteredProductsList.length / 10), // 每頁 10 筆
+  //   //   current_page: 1, // 預設從第 1 頁開始
+  //   //   has_pre: page > 1,
+  //   //   has_next: page < Math.ceil(filteredProductsList.length / 10),
+  //   //   category: "",
+  //   // });
 
   //   // console.log(page, filteredProducts);
   // };
@@ -249,7 +250,7 @@ export default function TravelSpots() {
                     } text-nowrap py-xl-4 py-md-3 py-2`}
                     href=""
                     onClick={(e) => {
-                      handleBannerBG(e, "全部");
+                      handleFilterProducts(e, "全部");
                     }}
                   >
                     全部
@@ -262,7 +263,7 @@ export default function TravelSpots() {
                     } travelSpotsSelectbutton  text-nowrap py-xl-4 py-md-3 py-2`}
                     href=""
                     onClick={(e) => {
-                      handleBannerBG(e, "亞洲");
+                      handleFilterProducts(e, "亞洲");
                     }}
                   >
                     亞洲
@@ -275,7 +276,7 @@ export default function TravelSpots() {
                     } travelSpotsSelectbutton  text-nowrap py-xl-4 py-md-3 py-2`}
                     href=""
                     onClick={(e) => {
-                      handleBannerBG(e, "歐洲");
+                      handleFilterProducts(e, "歐洲");
                     }}
                   >
                     歐洲
@@ -288,7 +289,7 @@ export default function TravelSpots() {
                     } travelSpotsSelectbutton  text-nowrap py-xl-4 py-md-3 py-2`}
                     href=""
                     onClick={(e) => {
-                      handleBannerBG(e, "中東");
+                      handleFilterProducts(e, "中東");
                     }}
                   >
                     中東
@@ -299,124 +300,140 @@ export default function TravelSpots() {
           </div>
           {/* 產品列表 */}
           <div className="row row-cols-sm-2 row-cols-1 productListTranslate">
-            {productList.map((product) => {
-              return (
-                <div key={product.id} className={`col`}>
-                  <a style={{ display: "block", height: "100%" }} href="">
-                    <div className="d-flex flex-column px-xl-6 px-lg-4 px-md-2 px-0 h-100">
-                      {/* 上方圖片區域 */}
-                      <div className="productListImgWrap overflow-hidden position-relative">
-                        <img
-                          className="productListImg"
-                          src={product.imageUrl}
-                          alt={product.title}
-                        />
-                        <DepartureTimeDecoration featuredItem={product} />
+            {isFilterProducts ? (
+              <h1>已篩選的資料</h1>
+            ) : (
+              productList.map((product) => {
+                return (
+                  <div key={product.id} className={`col`}>
+                    <a style={{ display: "block", height: "100%" }} href="">
+                      <div className="d-flex flex-column px-xl-6 px-lg-4 px-md-2 px-0 h-100">
+                        {/* 上方圖片區域 */}
+                        <div className="productListImgWrap overflow-hidden position-relative">
+                          <img
+                            className="productListImg"
+                            src={product.imageUrl}
+                            alt={product.title}
+                          />
+                          <DepartureTimeDecoration featuredItem={product} />
+                        </div>
+                        {/* 下方文字區域 */}
+                        <div className="mt-4 mb-2">
+                          <h3
+                            className="title-family travelSpotCardTitle text-neutral-black"
+                            style={{ whiteSpace: "pre-line" }}
+                          >
+                            {product.title}
+                          </h3>
+                        </div>
+                        <div className="mb-3">
+                          {product.description.split("\n").map((des, index) => {
+                            return (
+                              <p
+                                key={index}
+                                className={`${
+                                  index === 0 ? "mb-sm-0 mb-2" : ""
+                                } text-neutral-300 travelSpotCardDescription`}
+                              >
+                                {des}
+                              </p>
+                            );
+                          })}
+                        </div>
+                        <div className="mt-auto">
+                          <p
+                            style={{ fontSize: "14px" }}
+                            className="text-decoration-line-through text-neutral-200"
+                          >
+                            原價 NT {product.origin_price.toLocaleString()}
+                          </p>
+                          <p
+                            style={{ lineHeight: "1.2" }}
+                            className="text-secondary-200 travelSpotCardDiscountPrice fw-bold"
+                          >
+                            優惠價 NT {product.price.toLocaleString()}/
+                            {product.unit}
+                          </p>
+                        </div>
                       </div>
-                      {/* 下方文字區域 */}
-                      <div className="mt-4 mb-2">
-                        <h3
-                          className="title-family travelSpotCardTitle text-neutral-black"
-                          style={{ whiteSpace: "pre-line" }}
-                        >
-                          {product.title}
-                        </h3>
-                      </div>
-                      <div className="mb-3">
-                        {product.description.split("\n").map((des, index) => {
-                          return (
-                            <p
-                              key={index}
-                              className={`${
-                                index === 0 ? "mb-sm-0 mb-2" : ""
-                              } text-neutral-300 travelSpotCardDescription`}
-                            >
-                              {des}
-                            </p>
-                          );
-                        })}
-                      </div>
-                      <div className="mt-auto">
-                        <p
-                          style={{ fontSize: "14px" }}
-                          className="text-decoration-line-through text-neutral-200"
-                        >
-                          原價 NT {product.origin_price.toLocaleString()}
-                        </p>
-                        <p
-                          style={{ lineHeight: "1.2" }}
-                          className="text-secondary-200 travelSpotCardDiscountPrice fw-bold"
-                        >
-                          優惠價 NT {product.price.toLocaleString()}/
-                          {product.unit}
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              );
-            })}
+                    </a>
+                  </div>
+                );
+              })
+            )}
           </div>
           {/* 分頁元件 */}
           <div className="row my-15">
             <div className="col">
               <div className="d-flex justify-content-center">
                 <ul className="list-unstyled mb-0 d-flex align-items-center">
-                  <li>
-                    <a
-                      className={`leftArrow ${
-                        pagination.has_pre ? "" : "disabled"
-                      }  `}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        getProduct(pagination.current_page - 1);
-                      }}
-                    ></a>
-                  </li>
-                  {[...new Array(pagination.total_pages)].map((_, index) => {
-                    return (
-                      <li
-                        className={`${
-                          index === 0 ? "" : "paginationNumbersMargin"
-                        }`}
-                        key={`${index}_page`}
-                      >
+                  {isFilterProducts ? (
+                    <>
+                      <h3>已篩選的資料</h3>
+                      {filteredProductData.map((item) => item.title)}
+                    </>
+                  ) : (
+                    <>
+                      <h3>未篩選的原始資料</h3>
+                      <li>
                         <a
-                          className={`fw-bold paginationNumbers ${
-                            index + 1 === pagination.current_page
-                              ? "paginationActive"
-                              : ""
-                          }`}
-                          style={{
-                            padding: "4px 10px",
-                            fontSize: "20px",
-                            lineHeight: "1.2",
-                            borderRadius: "100px",
-                            cursor: "pointer",
-                            display: "block",
-                          }}
+                          className={`leftArrow ${
+                            pagination.has_pre ? "" : "disabled"
+                          }  `}
                           onClick={(e) => {
                             e.preventDefault();
-                            getProduct(index + 1);
+                            getProduct(pagination.current_page - 1);
                           }}
-                          href=""
-                        >
-                          {index + 1}
-                        </a>
+                        ></a>
                       </li>
-                    );
-                  })}
-                  <li>
-                    <a
-                      className={`rightArrow ${
-                        pagination.has_next ? "" : "disabled"
-                      }`}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        getProduct(pagination.current_page + 1);
-                      }}
-                    ></a>
-                  </li>
+                      {[...new Array(pagination.total_pages)].map(
+                        (_, index) => {
+                          return (
+                            <li
+                              className={`${
+                                index === 0 ? "" : "paginationNumbersMargin"
+                              }`}
+                              key={`${index}_page`}
+                            >
+                              <a
+                                className={`fw-bold paginationNumbers ${
+                                  index + 1 === pagination.current_page
+                                    ? "paginationActive"
+                                    : ""
+                                }`}
+                                style={{
+                                  padding: "4px 10px",
+                                  fontSize: "20px",
+                                  lineHeight: "1.2",
+                                  borderRadius: "100px",
+                                  cursor: "pointer",
+                                  display: "block",
+                                }}
+                                onClick={(e) => {
+                                  e.preventDefault();
+                                  getProduct(index + 1);
+                                }}
+                                href=""
+                              >
+                                {index + 1}
+                              </a>
+                            </li>
+                          );
+                        }
+                      )}
+                      <li>
+                        <a
+                          className={`rightArrow ${
+                            pagination.has_next ? "" : "disabled"
+                          }`}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            getProduct(pagination.current_page + 1);
+                          }}
+                        ></a>
+                      </li>
+                    </>
+                  )}
                 </ul>
               </div>
             </div>
