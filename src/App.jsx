d80ea590b5af9b59091ac474d25f1e074ff15c4b
mynewhome25@gmail.com <hailey@hailey.mshome.net>
2025-02-20 23:50:04 +0800

    fix(home): 處理header在windoes會出現x軸

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,85 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useReducer } from "react";
 // 匯入頁面(pages>index.jsx做匯入喔)
-import {Home,About,Cart,CartOrder,CartPayment,Account,TravelGuide,TravelSpots,TravelSpotsItem,TravelGuideItem, CompletePayment} from './pages';
+import {
+  Home,
+  About,
+  Cart,
+  CartOrder,
+  CartPayment,
+  Account,
+  TravelGuide,
+  TravelSpots,
+  TravelSpotsItem,
+  TravelGuideItem,
+  CompletePayment,
+} from "./pages";
 // 匯入元件(去components>index.jsx做匯入喔)
-import { BackTopBtn, Footer, Navbar } from './components';
+import { BackTopBtn, Footer, Navbar } from "./components";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import './scss/all.scss';
+import "./scss/all.scss";
+import { CartContext} from "./store/store";
 
 function App() {
   const location = useLocation(); // 儲存當前頁面
   const isNotBackTopPage = !(
-    location.pathname === '/cart' || 
-    location.pathname === '/account' || 
-    location.pathname.startsWith('/travelGuide/')
+    location.pathname === "/cart" ||
+    location.pathname === "/account" ||
+    location.pathname.startsWith("/travelGuide/")
   );
+
+  const cartReducer = useReducer((state, action) => {
+    const cartList = [...state.cartList]
+    console.log(cartList);
+    
+    switch(action.type){
+      case 'ADD_TO_CART':
+        cartList.push(action.payload)
+        return {
+          ...state,
+          cartList
+        }
+      default:
+        return state
+    }
+  }, {
+    cartList: []
+  })
+  
   return (
-    <div>
+    <CartContext.Provider value={cartReducer}>
       <Navbar />
       <div className="navbar-top">
-          <Routes>
-            <Route path='/' element={<Home />}></Route>                    {/* 首頁 */}
-            <Route path='/about' element={<About/>}></Route>               {/* 關於我們 */}
-            <Route path='/travelSpots' element={<TravelSpots />}/>     {/* 旅遊景點-商品列表 */}
-            <Route path="/travelSpots/:id" element={<TravelSpotsItem />} />         {/* ---各別單一商品-呈現 */}
-            <Route path='/travelGuide' element={<TravelGuide/>} />                {/* 攻略指南 */}
-            <Route path="/travelGuide/:id" element={<TravelGuideItem />} />   
-            <Route path='/cart' element={<Cart/>}></Route>                 {/* 購物車 */}
-            <Route path='/cartOrder' element={<CartOrder/>}></Route>                 {/* 購物車訂單 */}
-            <Route path='/cartPayment' element={<CartPayment/>}></Route>                 {/* 購物車支付頁 */}
-            <Route path='/completePayment' element={<CompletePayment/>}></Route>                 {/* 完成付款 */}
-            <Route path='/account' element={<Account/>}></Route>           {/* 登入註冊 */}
-          </Routes>
-        </div>
-        {isNotBackTopPage && <BackTopBtn />}
-      <Footer/>
-    </div>
-  )
+        <Routes>
+          <Route path="/" element={<Home />}></Route> {/* 首頁 */}
+          <Route path="/about" element={<About />}></Route> {/* 關於我們 */}
+          <Route path="/travelSpots" element={<TravelSpots />} />{" "}
+          {/* 旅遊景點-商品列表 */}
+          <Route path="/travelSpots/:id" element={<TravelSpotsItem />} />{" "}
+          {/* ---各別單一商品-呈現 */}
+          <Route path="/travelGuide" element={<TravelGuide />} />{" "}
+          {/* 攻略指南 */}
+          <Route path="/travelGuide/:id" element={<TravelGuideItem />} />
+          <Route path="/cart" element={<Cart />}></Route> {/* 購物車 */}
+          <Route path="/cartOrder" element={<CartOrder />}></Route>{" "}
+          {/* 購物車訂單 */}
+          <Route path="/cartPayment" element={<CartPayment />}></Route>{" "}
+          {/* 購物車支付頁 */}
+          <Route
+            path="/completePayment"
+            element={<CompletePayment />}
+          ></Route>{" "}
+          {/* 完成付款 */}
+          <Route path="/account" element={<Account />}></Route> {/* 登入註冊 */}
+        </Routes>
+      </div>
+      {isNotBackTopPage && <BackTopBtn />}
+      <Footer />
+    </CartContext.Provider>
+  );
 }
 
-export default App
-
+export default App;
--- src/pages/CartPayment.jsx
+++ src/pages/CartPayment.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../store/store";
 
 export default function CartPayment() {
+  const [state, dispatch] = useContext(CartContext)
   return (
     <>
       {/* 進度條 */}
@@ -8,13 +11,12 @@ export default function CartPayment() {
         <div className="row row-cols-4 text-center">
           <div className="col d-flex justify-content-center align-items-center flex-column py-md-4 py-3">
             <div
-              className="bg-primary-500 mb-4 rounded-circle"
+              className="bg-primary-500 mb-4 rounded-circle d-flex justify-content-center align-items-center"
               style={{ width: "32px", height: "32px" }}
             >
               <img
                 src="images/icon/check.svg"
                 alt=""
-                style={{ verticalAlign: "sub" }}
               />
             </div>
             <p className="text-primary-500 fs-md-9 fs-12">購物車明細</p>
@@ -22,13 +24,12 @@ export default function CartPayment() {
 
           <div className="col d-flex justify-content-center align-items-center flex-column py-md-4 py-3">
             <div
-              className="bg-primary-500 mb-4 rounded-circle"
+              className="bg-primary-500 mb-4 rounded-circle d-flex justify-content-center align-items-center"
               style={{ width: "32px", height: "32px" }}
             >
               <img
                 src="images/icon/check.svg"
                 alt=""
-                style={{ verticalAlign: "sub" }}
               />
             </div>
             <p className="text-primary-500 fs-md-9 fs-12">填寫資料</p>
