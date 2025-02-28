91bdb77a91aedbb926f95cab033730a52cc2f323
Kaya <kaya_chen@outlook.com>
2025-02-28 18:15:27 +0800

    fix(Cart.jsx):路由修正

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -153,7 +153,7 @@ export default function Cart() {
                     清空購物車品項
                   </button>
                   <Link
-                    to="/CartOrder"
+                    to="/cartOrder"
                     className="btn btn-secondary-200 fs-lg-7 fs-10 w-100"
                   >
                     下一步：填寫訂單
